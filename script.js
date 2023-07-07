6c2d846c6141bdd02648ee26402108ef85a4b4f7
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-07-07 16:08:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-function decimalToBinary(decimalNum) {
-  let binaryNum = ''; // Initialize an empty string for the binary number
-  let quotient = decimalNum; // Initialize the quotient to the decimal number
-
-  while (quotient !== 0) {
-    let remainder = quotient % 2; // Get the remainder of dividing the quotient by 2
-    binaryNum = remainder + binaryNum; // Add the remainder to the left of the binary number string
-    quotient = Math.floor(quotient / 2); // Divide the quotient by 2 and round down
-  }
-
-  return binaryNum; // Return the binary number as a string
+function decimalToBinary(num) {
+  // Write your code here
 }
+
+window.decimalToBinary = decimalToBinary;
